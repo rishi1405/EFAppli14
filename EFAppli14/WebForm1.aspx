427887fb9849eaf500cfb4a9c0249126982a61cc
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 14:54:24 +0530

    EFAppli14

---
+++ EFAppli14/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EFAppli14.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-family:Arial">
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text=" Assign WCF Course to Mike" />
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Remove John from SQL Server Course" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ EFAppli14/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EFAppli14
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            var query = from student in employeeDBContext.Students
+                                   from courses in student.Courses
+                                   select new
+                                   {
+                                       StudentName = student.StudentName,
+                                       CourseName = courses.CourseName
+                                   };
+            GridView1.DataSource = query.ToList();
+                                 GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            Course WCFCourse = employeeDBContext.Courses
+                .FirstOrDefault(x => x.CourseID == 4);
+
+            employeeDBContext.Students.FirstOrDefault(x => x.StudentID == 1)
+                .Courses.Add(WCFCourse);
+            employeeDBContext.SaveChanges();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            Course SQLServerCourse = employeeDBContext.Courses
+                .FirstOrDefault(x => x.CourseID == 3);
+
+            employeeDBContext.Students.FirstOrDefault(x => x.StudentID == 2)
+                .Courses.Remove(SQLServerCourse);
+            employeeDBContext.SaveChanges();
+        }
+    }
+}
